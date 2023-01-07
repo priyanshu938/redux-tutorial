ec688bc2d7a7bc725a2247fb1470ca2e48c688ec
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-01-07 21:41:27 +0530

    state changed,made use of useDispatch hook

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,12 +1,36 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "../state/index";
 
 const Shop = () => {
+  const dispatch = useDispatch();
+  const { depositMoney, withdrawMoney } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
+  const balance = useSelector((state) => state.amount);
+
   return (
     <div>
       <h2>Deposit/Withdraw money</h2>
-      <button className="btn btn-primary mx-2">-</button>
-      Update Balance
-      <button className="btn btn-primary mx-2">+</button>
+      <button
+        className="btn btn-primary mx-2"
+        onClick={() => {
+          withdrawMoney(100);
+        }}
+      >
+        -
+      </button>
+      Update Balance Rs {balance}
+      <button
+        className="btn btn-primary mx-2"
+        onClick={() => {
+          depositMoney(100);
+        }}
+      >
+        +
+      </button>
     </div>
   );
 };
