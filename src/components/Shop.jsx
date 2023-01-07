a7b3e98e38f1e7f3c0c7686f05ad85a4a99e7885
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-01-07 20:36:56 +0530

    initial commit

---
+++ src/components/Shop.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+    <h2>Buy Antiwar Shoes Rs50</h2>
+    <button className="btn btn-primary mx-2">-</button>
+    Add to cart
+    <button className="btn btn-primary mx-2">+</button>
+
+    </div>
+  )
+}
+
+export default Shop
